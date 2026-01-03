1f9691352eb8d3548da1215189ee148f1ee162a3
rusithhansana <rusithhansana.dev@gmail.com>
2026-01-03 11:30:05 +0530

    Add new app icons for iOS and android using `flutter_launcher_icons`

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,21 @@
+import 'package:firebase_app_check/firebase_app_check.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:fitgenie_app/firebase_options.dart';
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/adapters.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  await FirebaseAppCheck.instance.activate(
+    androidProvider: AndroidProvider.debug,
+    appleProvider: AppleProvider.debug,
+  );
+
+  await Hive.initFlutter();
+
   runApp(const MyApp());
 }
 
