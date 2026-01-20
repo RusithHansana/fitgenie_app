e035cca3a61278b7dd57aa6b5577d54c70d1514c
rusithhansana <rusithhansana.dev@gmail.com>
2026-01-20 18:22:31 +0530

    feat: Remove unused import of flutter_riverpod in app_router.dart

--- lib/routing/app_router.dart
+++ lib/routing/app_router.dart
@@ -1,6 +1,5 @@
 import 'package:fitgenie_app/features/auth/domain/user_model.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
